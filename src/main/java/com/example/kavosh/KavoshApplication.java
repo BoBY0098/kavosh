9a3dcdaff0852dbe94b2ce924579ae2c46ea7a3c
amirhosseinjavid <amirhosenjavid@aol.com>
2024-07-03 01:25:07 +0330

    dev : second commit

---
+++ src/main/java/com/example/kavosh/KavoshApplication.java
@@ -0,0 +1,13 @@
+package com.example.kavosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KavoshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KavoshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kavosh/KavoshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kavosh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KavoshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
